d1a0bd3011e1a6b7cdeb5ec9e739798096ab07f1
gihantha <gihantha01@gmail.com>
2024-08-28 09:50:56 +0530

    feat(admin-posts): include pages based on condition technique

--- admin/posts.php
+++ admin/posts.php
@@ -32,55 +32,32 @@
                     <div class="card">
                         <div class="row">
                             <div class="col-lg-12 p-4 m-2">
-                                <table class="table table-bordered table-hover">
-                                    <thead class="small">
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>Author</th>
-                                            <th>Title</th>
-                                            <th>Category</th>
-                                            <th>Status</th>
-                                            <th>Image</th>
-                                            <th>Tags</th>
-                                            <th>Comments</th>
-                                            <th>Date</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        global $connection;
+                               <?php
+                               if(isset($_GET['source'])){
+                                $source = $_GET['source'];
+                               } else {
+                                $source = '';
+                               }
 
-                                        $query = "SELECT * FROM posts";
-                                        $select_all_posts = mysqli_query($connection, $query);
-
-                                        while ($row = mysqli_fetch_assoc($select_all_posts)) {
-                                            $post_id = htmlspecialchars($row['post_id']);
-                                            $post_category_id = htmlspecialchars($row['post_category_id']);
-                                            $post_title = htmlspecialchars($row['post_title']);
-                                            $post_author = htmlspecialchars($row['post_author']);
-                                            $post_date = htmlspecialchars($row['post_date']);
-                                            $post_image = htmlspecialchars($row['post_image']);
-                                            $post_content = htmlspecialchars($row['post_content']);
-                                            $post_status = htmlspecialchars($row['post_status']);
-                                            $post_tags = htmlspecialchars($row['post_tags']);
-                                            $post_comment_count = htmlspecialchars($row['post_comment_count']);
-
-                                            echo "<tr class='small'>";
-                                            echo "<td>$post_id</td>";
-                                            echo "<td>$post_author</td>";
-                                            echo "<td>$post_title</td>";
-                                            echo "<td>$post_category_id</td>";
-                                            echo "<td>$post_status</td>";
-                                            echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-                                            echo "<td>$post_tags</td>";
-                                            echo "<td>$post_comment_count</td>";
-                                            echo "<td>$post_date</td>";
-                                            echo "</tr>";
-                                        }
-                                        ?>
-                                        
-                                    </tbody>
-                                </table>
+                               switch($source){
+                                    case '34';
+                                    echo "NICE 23";
+                                    break;
+                                    case '100';
+                                    echo "NICE 13";
+                                    break;
+                                    case '200';
+                                    echo "NICE 20";
+                                    break;
+                                    case '300';
+                                    echo "NICE 33";
+                                    break;
+                                    default:
+                                    include "includes/view_all_posts.php";
+                                    break;
+                               };
+                               
+                               ?>
                             </div>
                         </div>
                     </div>
